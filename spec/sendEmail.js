ee2545f4b8efe5e8798f60ad10628c0171f18570
saospina <sergos0617@gmail.com>
2019-05-22 17:42:08 -0500

    starting to search in google

--- spec/sendEmail.js
+++ spec/sendEmail.js
@@ -1,5 +1,10 @@
 describe('Sending emails through Gmail', function() {
     it('should open Google page', function () {
+        browser.waitForAngularEnabled(false);
+        browser.get('https://www.google.com/');
+        element(by.css("input[class='gLFyf gsfi']")).sendKeys('gmail');
+        element(by.css("input[class='gLFyf gsfi']")).sendKeys(Key.ENTER);
+        
         
     });
 });
